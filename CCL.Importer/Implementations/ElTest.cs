cc02f717cf895df8b854c409fc7b0cbbe7431340
Knsgf <kf177045@hotmail.com>
2026-08-01 11:24:50 +0300

    Catenary class retrievial test

--- CCL.Importer/Implementations/ElTest.cs
+++ CCL.Importer/Implementations/ElTest.cs
@@ -13,12 +13,22 @@ namespace CCL.Importer.Implementations
 {
 	internal class ElTest : SimComponent
 	{
+		//const string OCS_class_name = "electric_sim.catenary.overhead_equipment, electric_sim, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+		const string OCS_class_name = "electric_sim.catenary.overhead_equipment, electric_sim";
+		
 		private readonly float _f;
 		private float _t = 0.0f;
+		
+		private void test_load()
+		{
+			var t = Type.GetType(OCS_class_name, throwOnError: false);
+			Debug.Log($"ElTest {((t == null) ? "NULL" : t)}");
+		}
 		
 		public ElTest(ElTestDefinitionInternal def): base(def.ID)
 		{
 			_f = def.f;
+			test_load();
 		}
 
 		public override void Tick(float delta)
@@ -28,6 +38,7 @@ namespace CCL.Importer.Implementations
 			{
 				Debug.Log($"ElTest {_t}>={_f}");
 				_t -= _f;
+				test_load();
 			}
 		}
 	}
